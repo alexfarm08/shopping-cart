815f6571f437fced37e497bebce7e1a7497141d8
alexfarm08 <209876587+alexfarm08@users.noreply.github.com>
2025-12-31 15:13:13 -0800

    added shop page and styles for shop page

--- src/componets/Shop.jsx
+++ src/componets/Shop.jsx
@@ -1,10 +1,38 @@
-
+import { useState, useEffect } from "react";
+import Card from "./Card";
+import styles from "../css-modules/Shop.module.css";
 
 function Shop() {
+
+    const [products, setProduct] = useState([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const res = await fetch("https://fakestoreapi.com/products");
+                const data = await res.json();
+                setProduct(data);
+            }
+            catch(e) {
+                console.log(e);
+            } 
+        }
+        fetchData();
+    }, []);
+
     return (
-        <>
-            <h1>Shopping Page</h1>
-        </>
+        <div className={styles.shop}>
+            <h2 className={styles.shopHeading}>Shop our great fake products</h2>
+            <div className={styles.productsDiv}>
+                {products && products.length > 0 ? (
+                    products.map(product => (
+                        <Card className={styles.productsCard} key={product.id} image={product.image} title={product.title} description={product.description} price={product.price} />
+                    ))
+                ) : (
+                    <p className={styles.loading}>Loading...</p>
+                )}
+            </div>
+        </div>
     )
 }
 
